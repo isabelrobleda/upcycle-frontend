1794e1b86de3c10237f65d986b0162f836ddf4b4
Isabel Robleda <isabel.robleda@gmail.com>
2024-04-18 23:35:00 +0200

    change back to http localhost

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -462,7 +462,7 @@ function Form() {
 
     try {
       const response = await axios.post(
-        "https://localhost:5005/api/vendor-form",
+        "http://localhost:5005/api/vendor-form",
         formDataToSend
       );
       return response.data;
